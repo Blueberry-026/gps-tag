8a7c7cd79fcfd0e9c4289f1c72b15b003aa1fd4e
Blueberry-026 <65850866+Blueberry-026@users.noreply.github.com>
2020-05-24 13:18:41 +0200

    Mise au propre

--- gps-tag.py
+++ gps-tag.py
@@ -3,75 +3,94 @@ import sys
 import os
 import glob
 
-jpgFile = "test"
-gpxFile = "plymouth.gpx"
+photoRep = "e:\\uk\\Python\\seb\\photos"
+logFile = photoRep + "\\gps-tag.csv"
 
-offset = 500
-
-srcRep = "\\uk\\Photos"
-dstRep = "\\uk\\Python\\Exif\\fixed"
+SEP = ';'
 
 try:
-    for jpgFile in glob.glob(srcRep+'\\*.jpg'):
-        print "Traitement de " + jpgFile
-        try:
-            ef = JpegFile.fromFile(jpgFile)
+    fOut = open(logFile,"w")
+    ligne = "Repertoire"    + SEP + \
+            "Fichier"       + SEP + \
+            "Date"          + SEP + \
+            "Heure"         + SEP + \
+            "Marque"        + SEP + \
+            "Modele"        + SEP + \
+            "Exposition"    + SEP + \
+            "Aperture"      + SEP + \
+            "Equiv ISO"     + SEP + \
+            "Dim X"         + SEP + \
+            "Dim Y"         + SEP + \
+            "Latitude"      + SEP + \
+            "Longitude"     + '\n'
+    fOut.write(ligne)
 
-            primary = ef.get_exif().get_primary()
-            dt = primary.ExtendedEXIF.DateTimeOriginal[:10]
-            dt = dt.replace(":","-")
-
-            tm = primary.ExtendedEXIF.DateTimeOriginal[11:]
-            tm = tm.split(":")
-
-            # dans le JPEG, 12h heure FR = 13h heure UK
-
-            j_hr=int(tm[0])
-            j_mn=int(tm[1])
-            j_sc=int(tm[2])
-
-            # il y a 500 s d'ecart entre heure GPS et heure GPS
-
-            j_time = (j_hr*3600) + (j_mn*60) + j_sc + offset
-
-            posLat = 0
-            posLon = 0
-            diffTime = 4*3600
-
-            for gpxLine in open(gpxFile, 'r'):
-                if "<trkpt " in gpxLine:
-                    gpxPos = gpxLine
-                if (dt in gpxLine):
-                    # Un point trouv? ? la bonne date : chercher si il est
-                    # plus proche en temps du dernier trouv?
-                    # dans le gpx, 12h heure FR = 13h heure UK
-                    gpxLine = gpxLine.strip()
-                    g_hr=int(gpxLine[17:19])
-                    g_mn=int(gpxLine[20:22])
-                    g_sc=int(gpxLine[23:25])
-                    g_time = (g_hr*3600) + (g_mn*60) + g_sc
-
-                    if (abs(j_time - g_time) < diffTime):
-                        diffTime = abs(j_time - g_time)
-                        latlon = gpxPos.split('\"')
-                        posLat = latlon[1]
-                        posLon = latlon[3]
-            if ((posLat==0) and (posLon==0)):
-                print "   => pas de position trouv?e ? cette heure"
+    path = photoRep
+    for dirname, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            jpgFile = os.path.join(dirname, filename)
+            fext = os.path.splitext(jpgFile)[1]
+            if fext.upper() == '.JPG':
+                print "Traitement de " + jpgFile
             else:
-                # Ecriture du tag GPS dans l'EXIF de la photo avec les
-                # meilleures lat/lon trouv?es
+                print "Ignore " + jpgFile
+                continue
 
-                ef.set_geo(float(posLat), float(posLon))
-                print "   => diffTime=",diffTime
+            try:
+                ef = JpegFile.fromFile(jpgFile)
 
-                base=os.path.basename(jpgFile)
-                print "   => Enregistre sous : " + dstRep + "\\" + base
-                ef.writeFile(dstRep + "\\" + base)
-            print "   => ok"
-        except:
-            print "   => FAILED"
+                primary = ef.get_exif().get_primary()
+                dt = primary.ExtendedEXIF.DateTimeOriginal[:10]
+                tm = primary.ExtendedEXIF.DateTimeOriginal[11:]
 
+                try:
+                    GPSpos = ef.get_geo()
+                except:
+                    GPSpos = (0,0)
+
+                exifTags = primary.entries
+                exifExtended = primary.ExtendedEXIF.entries
+                for tag in exifTags :
+                    if tag[0] == 0x10F:
+                       xf_Marque = tag[2]
+                    if tag[0] == 0x110:
+                       xf_Model = tag[2]
+                    if tag[0] == 0x9201:
+                       xf_Model = tag[2]
+
+                xf_DimX     = "error"
+                xf_DimY     = "error"
+                xf_Exposure = "error"
+                xf_Aperture = "error"
+                xf_Iso      = "error"
+                try:
+                   xf_DimX     = str(primary.ExtendedEXIF.PixelXDimension[0])
+                   xf_DimY     = str(primary.ExtendedEXIF.PixelYDimension[0])
+                   xf_Exposure = str(primary.ExtendedEXIF.ExposureTime[0])
+                   xf_Aperture = str(primary.ExtendedEXIF.MaxApertureValue[0])
+                   xf_Iso      = str(primary.ExtendedEXIF.ISOSpeedRatings[0])
+                except:
+                   print "   => EXIF fields error"
+
+                ligne = dirname                        + SEP + \
+                        os.path.basename(jpgFile)      + SEP + \
+                        dt                             + SEP + \
+                        tm                             + SEP + \
+                        xf_Marque                      + SEP + \
+                        xf_Model                       + SEP + \
+                        xf_Exposure                    + SEP + \
+                        xf_Aperture                    + SEP + \
+                        xf_Iso                         + SEP + \
+                        xf_DimX                        + SEP + \
+                        xf_DimY                        + SEP + \
+                        str(GPSpos[0])                 + SEP + \
+                        str(GPSpos[1])                 + '\n'
+
+                fOut.write(ligne)
+                print "   => "+ligne
+            except:
+                print "   => FAILED"
+    fOut.close()
 except IOError:
-   type, value, traceback = sys.exc_info()
-   print >> sys.stderr, "Error saving file:", value
+    type, value, traceback = sys.exc_info()
+    print >> sys.stderr, "Error parsing files"
